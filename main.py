3dff0cc1ac5c0e878c0e9758668d6cac833b1beb
SEOYUL LEE <lsy254436@gmail.com>
2026-06-10 20:18:58 +0900

    Add explanation for MBTI result display

--- main.py
+++ main.py
@@ -384,6 +384,16 @@ if __name__ == "__main__":
     print("\033[H\033[J", end="")
     time.sleep(1.2)
     print(f"당신의 연애 스타일 코드는 {mbti_result}입니다.")
+    
+    time.sleep(1.2)
+    print("\n집돌이(H) vs 야외형(O)\n\n다정형(K) vs 무심형(M)\n\n회피형(A) vs 돌진형(D)\n\n집착형(J) vs 방치형(B)\n\n")
+    time.sleep(1.5)
+    print(".")
+    time.sleep(1.7)
+    print(".")
+    time.sleep(1.8)
+    print(".")
+    time.sleep(1)
 
     explainer = get_result_module()
     explain_result = explainer.get_result(mbti_result)
